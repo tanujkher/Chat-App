bafc0c2da02308c9a7035f536224c08bf31bd377
tanujkher <tanujkher15@gmail.com>
2020-03-22 07:54:21 +0530

    You feature for self send messages and enter keyup feature

--- public/script.js
+++ public/script.js
@@ -3,10 +3,38 @@ let socket = io()
 $(() => {
     let btnSend = $('#btnSend')
     let ulMsgList = $('#ulMsgList')
+    let inpMsg = $('#inpMsg')
+
+    function send() {
+        socket.emit('msg_send', { msg: inpMsg.val(), user: socket.id })
+        inpMsg.val('')
+    }
+
+    inpMsg.keyup((event) => {
+        if(event.keyCode == '13'){
+            send()
+        }
+    })
 
     btnSend.click(() => {
-        let inpMsg = $('#inpMsg').val()
-        socket.emit('msg_send', {msg: inpMsg})
-        $('#inpMsg').val('')
+        send()
+    })
+    
+    socket.on('msg_rcvd', (data) => {
+        if(data.user == socket.id){
+            ulMsgList.append(`
+            <li class="list-group-item  list-group-item-success d-flex justify-content-between align-items-center">
+                ${data.msg}
+                <span class="badge badge-primary badge-pill">You</span>
+            </li>
+        `)
+        }else{
+            ulMsgList.append(`
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <span class="badge badge-primary badge-pill">${data.user}</span>
+                ${data.msg}
+            </li>
+        `)
+        }
     })
 })
